67671a8d51ecc839dfc2d715bb85e624a909ef48
Laxman Reddy <158175243+reddylaxman@users.noreply.github.com>
2024-05-23 14:33:34 +0530

    Update connectMongoDB.js

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECT_URL);
+    await mongoose.connect(mongodb+srv://aslearner:laxman_133@cluster0.a2daae5.mongodb.net/mern?retryWrites=true&w=majority&appName=Cluster0);
     console.log("Connect to MongoDB successfully");
   } catch (error) {
     console.log("Connection failed");
